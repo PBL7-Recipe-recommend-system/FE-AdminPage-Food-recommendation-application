7e74d723d5f887b393b57ffa9e65babbef54e71e
Huỳnh Lê Đắc Tài <dactai2k2@gmail.com>
2024-06-28 05:55:40 +0700

    Change the homepage route

--- src/pages/recipes/components/ingredient-table/ingredient-table-action.tsx
+++ src/pages/recipes/components/ingredient-table/ingredient-table-action.tsx
@@ -1,12 +1,11 @@
 import PopupModal from '@/components/shared/popup-modal';
-import TableSearchInput from '@/components/shared/table-search-input';
 import IngredientsCreateForm from '@/pages/recipes/components/ingredients-forms/ingredient-create-form';
 
 export default function IngredientTableActions() {
   return (
     <div className="flex items-center justify-between py-5">
       <div className="flex flex-1 gap-4">
-        <TableSearchInput placeholder="Search Ingredient Here" />
+        {/* <TableSearchInput placeholder="Search Ingredient Here" /> */}
       </div>
       <div className="flex gap-3">
         <PopupModal
--- src/pages/recipes/components/instruction-table/instruction-table-action.tsx
+++ src/pages/recipes/components/instruction-table/instruction-table-action.tsx
@@ -1,12 +1,11 @@
 import PopupModal from '@/components/shared/popup-modal';
-import TableSearchInput from '@/components/shared/table-search-input';
 import InstructionCreateForm from '@/pages/recipes/components/instruction-forms/instruction-create-form';
 
 export default function InstructionTableActions() {
   return (
     <div className="flex items-center justify-between py-5">
       <div className="flex flex-1 gap-4">
-        <TableSearchInput placeholder="Search Step Here" />
+        {/* <TableSearchInput placeholder="Search Step Here" /> */}
       </div>
       <div className="flex gap-3">
         <PopupModal
